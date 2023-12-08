d97f608870f5ffffeffc73d96d0d0867bb301f50
RamzyDendene <105855908+RamzyDendene@users.noreply.github.com>
2023-12-08 11:03:29 +0100

    Create plantList.js

---
+++ plantList.js
@@ -0,0 +1,78 @@
+
+import igloves from '../assets/IglovesBoite.jpg'
+import asap from '../assets/boite nivinyl.jpg'
+import mumu from '../assets/mumu boite.jpg'
+export const plantList = [
+	{
+		marque: 'iGloves',
+		name:'Gants en Nitrile',
+		sub_cat: 'Resorbable',
+		category:'Nitrile',
+		id: '1ed',
+		bestSale: true,
+		taille:'',
+		light: 2,
+		water: 3,
+		cover: igloves,
+		price: 14500,
+		conditionnement:"100 gants par boite",
+		description:"Gant en nitrile 100% non poudré",
+		stockData:40,
+		sizes: ["M-Carton de 10 boites"]
+
+	},
+	{
+		marque: 'ASAP',
+		name:'Gant Nivinyl bleu',
+		sub_cat: 'Non-Resorbable',
+		category:'Nitrile/Vinyl',
+		id: '2ed',
+		bestSale: true,
+		taille:'',
+		light: 2,
+		water: 3,
+		cover: asap,
+		price: 12500,
+		conditionnement:12,
+		description:"Les gants en matière mixte de nitrile et vinyle offrent une solution polyvalente pour diverses applications nécessitant une protection des mains. Fabriqués sans poudre, ces gants garantissent un environnement de travail sans résidus indésirables. La combinaison de nitrile et de vinyle confère une résistance accrue aux perforations, aux produits chimiques et aux huiles, tout en assurant une flexibilité et un confort optimaux. Disponibles en teintes élégantes de bleu ou de noir, ces gants allient fonctionnalité et esthétique, répondant ainsi aux normes de sécurité tout en offrant une solution pratique et attrayante pour les professionnels de divers secteurs.",
+		stockData:40,
+		sizes: ["S","M","L"]
+
+	},
+
+	{
+		marque: 'ASAP',
+		name:'Gant Nivinyl noir',
+		sub_cat: 'Non-Resorbable',
+		category:'Nitrile/Vinyl',
+		id: '3ed',
+		bestSale: true,
+		taille:'',
+		light: 2,
+		water: 3,
+		cover: asap,
+		price: 12500,
+		conditionnement:12,
+		description:"Gant d'examen en nitrile et vinyl non poudré, ambidextre",
+		stockData:40,
+		sizes: ["S","M","L"]
+	},
+	{
+		marque:'Mumu',
+		name: 'Gant en Nitrile ',
+		sub_cat: 'Resorbable',
+		category:'Nitrile',
+		id: '4ed',
+		bestSale: true,
+		taille:'',
+		light: 2,
+		water: 3,
+		cover: mumu,
+		price: 15000,
+		conditionnement:36,
+		description:'',
+		stockData:40,
+		sizes: ["S"]
+	},
+	
+]
